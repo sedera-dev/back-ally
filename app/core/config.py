bd3a3593cbad2c3bb04d5016cc6804cd06c1e697
sedera <sederafanomezana@gmail.com>
2025-02-13 10:31:02 +0300

    Token jwt

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,14 @@
-import os
-import environ
 
-env = environ.Env()
-environ.Env.read_env()
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Settings:
-    PROJECT_NAME: str = env('PROJECT_NAME')
-    SECRET_KEY: str = env('SECRET_KEY')
-    ALGORITHM: str = env('ALGORITHM')
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = env('ACCESS_TOKEN_EXPIRE_MINUTES')
+    PROJECT_NAME: str = getenv('PROJECT_NAME')
+    SECRET_KEY: str = getenv('SECRET_KEY')
+    ALGORITHM: str = getenv('ALGORITHM')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
 
 settings = Settings()
